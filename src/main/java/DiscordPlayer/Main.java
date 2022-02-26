38f87f8d34553c96d85c5d271fb4a3012a775690
alexc <alejandrocasanovalopez1@gmail.com>
2022-02-27 00:14:21 +0100

    Added search in YT and reproduce songs Added message sending when adding songs to queue Moved all commands to static{} block and all discord variables to global

--- src/main/java/DiscordPlayer/Main.java
+++ src/main/java/DiscordPlayer/Main.java
@@ -1,36 +1,30 @@
 package DiscordPlayer;
 
+import com.google.api.services.youtube.model.SearchResult;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
-import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Member;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.entity.channel.VoiceChannel;
 import discord4j.voice.AudioProvider;
-import discord4j.voice.VoiceConnection;
-import reactor.core.publisher.Mono;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     private static final Map<String, Command> commands = new HashMap<String, Command>();
+    static final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+    static final AudioPlayer player = playerManager.createPlayer();
+    static final TrackScheduler scheduler = new TrackScheduler(player);
+    static final AudioProvider provider = new LavaPlayerAudioProvider(player);
 
     public static void main(final String[] args) {
         // Creates AudioPlayer instances and translates URLs to AudioTrack instances
-        final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
 
 // This is an optimization strategy that Discord4J can utilize.
 // It is not important to understand
@@ -40,10 +34,8 @@ public class Main {
         AudioSourceManagers.registerRemoteSources(playerManager);
 
 // Create an AudioPlayer so Discord4J can receive audio data
-        final AudioPlayer player = playerManager.createPlayer();
 
 // We will be creating LavaPlayerAudioProvider in the next step
-        AudioProvider provider = new LavaPlayerAudioProvider(player);
         commands.put("join", event -> {
             final Member member = event.getMember().orElse(null);
             if (member != null) {
@@ -58,24 +50,7 @@ public class Main {
                 }
             }
         });
-        final TrackScheduler scheduler = new TrackScheduler(player);
-        commands.put("play", event -> {
-            final String content = event.getMessage().getContent().toString();
-            final List<String> command = Arrays.asList(content.split(" "));
-            System.out.println(command.get(1).toString());
-            scheduler.setEvent(event);
-            playerManager.loadItem(command.get(1), scheduler);
 
-        });
-        commands.put("skip", event -> {
-            scheduler.skip();
-        });
-        commands.put("queue", event -> {
-            scheduler.showQueue();
-        });
-        commands.put("clear", event -> {
-           scheduler.clearQueue();
-        });
         final GatewayDiscordClient client = DiscordClientBuilder.create("ODkzNTI4NDk2ODQyODkxMjY0.YVcxQg.EXtrzU6MjcsFVVyhv6ZUOZNMv9E").build().login().block();
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 // subscribe is like block, in that it will *request* for action
@@ -95,10 +70,38 @@ public class Main {
                     }
                 });
         client.onDisconnect().block();
-
     }
 
     static {
+        commands.put("play", event -> {
+            final String content = event.getMessage().getContent().toString();
+            final List<String> command = Arrays.asList(content.split(" " ));
+            if (command.get(1).startsWith("https://www.youtube.com/watch?v=")) {
+                System.out.println(command.get(1).toString());
+                scheduler.setEvent(event);
+                playerManager.loadItem(command.get(1), scheduler);
+            } else {
+                String url = "https://www.youtube.com/watch?v=";
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < command.size(); i++) {
+                    if(i != 0) {
+                        sb.append(command.get(i) + " ");
+                    }
+                }
+                SearchResult video = YoutubeSearch.getVideoInfo(sb.toString());
+                scheduler.setEvent(event);
+                playerManager.loadItem(url + video.getId().getVideoId(), scheduler);
+            }
+        });
+        commands.put("skip", event -> {
+            scheduler.skip();
+        });
+        commands.put("queue", event -> {
+            scheduler.showQueue();
+        });
+        commands.put("clear", event -> {
+            scheduler.clearQueue();
+        });
         commands.put("ping", event -> event.getMessage().getChannel().block().createMessage("Pong!").block());
     }
 }
