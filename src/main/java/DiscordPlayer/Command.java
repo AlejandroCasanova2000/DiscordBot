159a719d68c9029fd468c354b9a4c3adf2c8b5bf
alexc <alejandrocasanovalopez1@gmail.com>
2022-02-27 15:35:52 +0100

    Added .gitignore and applicacion.properties for storing keys

--- src/main/java/DiscordPlayer/Command.java
+++ src/main/java/DiscordPlayer/Command.java
@@ -2,6 +2,8 @@ package DiscordPlayer;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 
+import java.io.IOException;
+
 public interface Command {
-    void execute(MessageCreateEvent event);
+    void execute(MessageCreateEvent event) throws IOException;
 }
