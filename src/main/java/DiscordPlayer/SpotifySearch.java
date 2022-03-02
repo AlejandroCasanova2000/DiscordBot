5b338428cc7558c5d11192ba40a12612611dfeed
alexc <alejandrocasanovalopez1@gmail.com>
2022-03-02 18:39:39 +0100

    Added SpotifySearch SpotifySearch is a playlist to get tracknames from a Spotify playlist

---
+++ src/main/java/DiscordPlayer/SpotifySearch.java
@@ -0,0 +1,40 @@
+package DiscordPlayer;
+
+import com.google.api.client.json.Json;
+import org.apache.hc.core5.http.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SpotifySearch {
+
+    public static List<String> getPlaylistTrackNames(String playlistURL) throws IOException, ParseException, SpotifyWebApiException {
+        InputStream input = new FileInputStream("application.properties");
+        Properties properties = new Properties();
+        properties.load(input);
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken(properties.getProperty("spotifyToken"))
+                .build();
+        GetPlaylistsItemsRequest playlist = spotifyApi.getPlaylistsItems(playlistURL)
+                .fields("items(track.name)").build();
+        JSONObject json = new JSONObject(playlist.getJson());
+        JSONArray tracks = json.getJSONArray("items");
+        System.out.println(json.toString());
+        ArrayList<String> tracksNames = new ArrayList<String>();
+        for (int i = 0; i < tracks.length(); i++) {
+            tracksNames.add(tracks.getJSONObject(i).getJSONObject("track").getString("name"));
+        }
+        return tracksNames;
+    }
+}
