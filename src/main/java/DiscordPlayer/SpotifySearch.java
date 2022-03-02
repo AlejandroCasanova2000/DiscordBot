0863b07ba97b7532151cff7f223731e202773121
alexc <alejandrocasanovalopez1@gmail.com>
2022-03-03 00:04:16 +0100

    Configured SpotifySearch with accessToken and refreshToken Both tokens are in application.properties

--- src/main/java/DiscordPlayer/SpotifySearch.java
+++ src/main/java/DiscordPlayer/SpotifySearch.java
@@ -6,6 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
 
 import java.io.FileInputStream;
@@ -13,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -25,12 +27,12 @@ public class SpotifySearch {
         properties.load(input);
         SpotifyApi spotifyApi = new SpotifyApi.Builder()
                 .setAccessToken(properties.getProperty("spotifyToken"))
+                .setRefreshToken(properties.getProperty("spotifyRefresh"))
                 .build();
         GetPlaylistsItemsRequest playlist = spotifyApi.getPlaylistsItems(playlistURL)
                 .fields("items(track.name)").build();
         JSONObject json = new JSONObject(playlist.getJson());
         JSONArray tracks = json.getJSONArray("items");
-        System.out.println(json.toString());
         ArrayList<String> tracksNames = new ArrayList<String>();
         for (int i = 0; i < tracks.length(); i++) {
             tracksNames.add(tracks.getJSONObject(i).getJSONObject("track").getString("name"));
